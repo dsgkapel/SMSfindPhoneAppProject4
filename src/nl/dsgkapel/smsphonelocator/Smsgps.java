2f37383835248b70677dfe673affcaee255f1ea1
Olav1 <olavzeeuw@gmail.com>
2014-06-12 14:02:43 +0200

    Kleine verandering

--- src/nl/dsgkapel/smsphonelocator/Smsgps.java
+++ src/nl/dsgkapel/smsphonelocator/Smsgps.java
@@ -33,44 +33,30 @@ public class Smsgps extends Service {
 	}
 
 	private final BroadcastReceiver receiver = new BroadcastReceiver() {
+		
+	    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+	    private static final String TAG = "SMSBroadcastReceiver";
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			final Bundle bundle = intent.getExtras();
+	        Log.i(TAG, "Intent recieved: " + intent.getAction());
 
-			try {
-
-				if (bundle != null) {
-
-					final Object[] pdusObj = (Object[]) bundle.get("pdus");
-
-					for (int i = 0; i < pdusObj.length; i++) {
-
-						SmsMessage currentMessage = SmsMessage
-								.createFromPdu((byte[]) pdusObj[i]);
-						String phoneNumber = currentMessage
-								.getDisplayOriginatingAddress();
-
-						String senderNum = phoneNumber;
-						String message = currentMessage.getDisplayMessageBody();
-
-						Log.i("SmsReceiver", "senderNum: " + senderNum
-								+ "; message: " + message);
-
-						// Show alert
-						int duration = Toast.LENGTH_LONG;
-						Toast toast = Toast
-								.makeText(context, "senderNum: " + senderNum
-										+ ", message: " + message, duration);
-						toast.show();
-
-					} // end for loop
-				} // bundle is null
-
-			} catch (Exception e) {
-				Log.e("SmsReceiver", "Exception smsReceiver" + e);
-
-			}
+	        if (intent.getAction() == SMS_RECEIVED) {
+	            Bundle bundle = intent.getExtras();
+	            if (bundle != null) {
+	                Object[] pdus = (Object[]) bundle.get("pdus");
+	                final SmsMessage[] messages = new SmsMessage[pdus.length];
+	                for (int i = 0; i < pdus.length; i++) {
+	                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+	                }
+	                if (messages.length > -1) {
+	                    Log.i(TAG,
+	                            "Message recieved: " + messages[0].getMessageBody());
+	                    CharSequence text = "SMS Received";
+	                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+	                }
+	            }
+	        }
 		}
 	};
 
