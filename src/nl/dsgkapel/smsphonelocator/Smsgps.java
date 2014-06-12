44a7621ec8201ae09906d74d4e2678e85f20628c
Olav1 <olavzeeuw@gmail.com>
2014-06-12 13:53:26 +0200

    De service kan nu reageren op binnengekomen SMS berichten

--- src/nl/dsgkapel/smsphonelocator/Smsgps.java
+++ src/nl/dsgkapel/smsphonelocator/Smsgps.java
@@ -1,9 +1,14 @@
 package nl.dsgkapel.smsphonelocator;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class Smsgps extends Service {
@@ -19,10 +24,55 @@ public class Smsgps extends Service {
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 		
-		SmsReceiver sms = new SmsReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.addAction("android.provider.Telephony.SMS_RECEIVED");
 		
+		registerReceiver(receiver, filter);
+
 		return START_STICKY;
 	}
+
+	private final BroadcastReceiver receiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			final Bundle bundle = intent.getExtras();
+
+			try {
+
+				if (bundle != null) {
+
+					final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+					for (int i = 0; i < pdusObj.length; i++) {
+
+						SmsMessage currentMessage = SmsMessage
+								.createFromPdu((byte[]) pdusObj[i]);
+						String phoneNumber = currentMessage
+								.getDisplayOriginatingAddress();
+
+						String senderNum = phoneNumber;
+						String message = currentMessage.getDisplayMessageBody();
+
+						Log.i("SmsReceiver", "senderNum: " + senderNum
+								+ "; message: " + message);
+
+						// Show alert
+						int duration = Toast.LENGTH_LONG;
+						Toast toast = Toast
+								.makeText(context, "senderNum: " + senderNum
+										+ ", message: " + message, duration);
+						toast.show();
+
+					} // end for loop
+				} // bundle is null
+
+			} catch (Exception e) {
+				Log.e("SmsReceiver", "Exception smsReceiver" + e);
+
+			}
+		}
+	};
 
 	@Override
 	public IBinder onBind(Intent intent) {
