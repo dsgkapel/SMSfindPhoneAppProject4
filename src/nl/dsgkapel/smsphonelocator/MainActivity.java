916a0ea04d3f19625068dd3011f71e0b46c314e7
Olav1 <olavzeeuw@gmail.com>
2014-06-16 15:30:14 +0200

    Stuurt nu GPS coordinaten via SMS

--- src/nl/dsgkapel/smsphonelocator/MainActivity.java
+++ src/nl/dsgkapel/smsphonelocator/MainActivity.java
@@ -10,27 +10,33 @@ import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
+	
+	
 	public static boolean on;
-	public static String a = "Turn app on";
-
+	String a = "Turn app on";
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		Button onoff = (Button) findViewById(R.id.turnon);
-
-		if (Smsgps.running == false || a == "turn app on") {
+		Intent smsservice = new Intent(this, Smsgps.class);
+		
+		if (a == "Turn app on") {
 			a = "Turn app on";
 			onoff.setText(a);
 			
 		} else {
-			a = "Turn app on";
+			a = "Turn app off";
 			onoff.setText(a);
 		}
 
 	}
 
+
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -50,8 +56,9 @@ public class MainActivity extends ActionBarActivity {
 
 	public void turnon(View view) {
 		Button onoff = (Button) findViewById(R.id.turnon);
+		Intent smsservice = new Intent(this, Smsgps.class);
 		if (Smsgps.running == false || a == "turn app on") {
-			startService(new Intent(this, Smsgps.class));
+			startService(smsservice);
 			a = "Turn app off";
 			onoff.setText(a);
 		}
--- src/nl/dsgkapel/smsphonelocator/Smsgps.java
+++ src/nl/dsgkapel/smsphonelocator/Smsgps.java
@@ -1,12 +1,18 @@
 package nl.dsgkapel.smsphonelocator;
 
-import android.app.PendingIntent;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
@@ -16,12 +22,13 @@ import android.widget.Toast;
 public class Smsgps extends Service {
 
 	public static boolean running = false;
-	
+	LocationManager lm;
 
-
-	public Smsgps() {
-
-	}
+	public static String latstring;
+	public static String longstring;
+	private static final String TAG = "coordinates";
+	private Timer timer;
+	String PROVIDER = LocationManager.GPS_PROVIDER;
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
@@ -30,29 +37,47 @@ public class Smsgps extends Service {
 		int duration = Toast.LENGTH_SHORT;
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
-
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		IntentFilter filter = new IntentFilter();
 		filter.addAction("android.provider.Telephony.SMS_RECEIVED");
 		registerReceiver(receiver, filter);
 		running = true;
+		Location location = lm.getLastKnownLocation(PROVIDER);
+
+		saveLoc(location);
 
 		return START_STICKY;
+	}
+
+	public void saveLoc(Location l) {
+		Log.v(TAG, "Method running");
+
+		lm.requestLocationUpdates(PROVIDER, // provider
+				0, // minTime in ms
+				0, // minDistance in m
+				ll); // LocationListener
+		latstring = "Latitude: " + l.getLatitude();
+		longstring = " Longitude: " + l.getLongitude();
+		Log.v(TAG, latstring + longstring);
+		lm.removeUpdates(ll);
+
 	}
 
 	public void onDestroy() {
 		Context context = getApplicationContext();
 		running = false;
+		lm.removeUpdates(ll);
 		CharSequence text = "Service stopped";
 		int duration = Toast.LENGTH_SHORT;
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
-		MainActivity.a = "Turn app on";
+
 	}
 
 	private final BroadcastReceiver receiver = new BroadcastReceiver() {
-		String phonenr;
-		String messageText = "It works!";
 
+		String phonenr;
+		String messageText = latstring + longstring;
 		private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 		private static final String TAG = "SMSBroadcastReceiver";
 
@@ -63,16 +88,17 @@ public class Smsgps extends Service {
 			if (intent.getAction() == SMS_RECEIVED) {
 				Bundle bundle = intent.getExtras();
 				if (bundle != null) {
-					
+
 					Object[] pdus = (Object[]) bundle.get("pdus");
 					final SmsMessage[] messages = new SmsMessage[pdus.length];
-					
+
 					for (int i = 0; i < pdus.length; i++) {
 						messages[i] = SmsMessage
 								.createFromPdu((byte[]) pdus[i]);
 						phonenr += messages[i].getOriginatingAddress();
 					}
 					if (messages.length > -1) {
+
 						// Hier komt de code voor het reageren op een sms
 						Log.i(TAG,
 								"Message recieved: "
@@ -80,13 +106,40 @@ public class Smsgps extends Service {
 						CharSequence text = "SMS Received";
 						Toast.makeText(context, text, Toast.LENGTH_SHORT)
 								.show();
-						
+
 						SmsManager sms = SmsManager.getDefault();
-						sms.sendTextMessage(phonenr, null, messageText, null, null);
+						sms.sendTextMessage(phonenr, null, messageText, null,
+								null);
 
 					}
 				}
 			}
+		}
+	};
+
+	private LocationListener ll = new LocationListener() {
+
+		@Override
+		public void onLocationChanged(Location location) {
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stubmessageNumber
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+
 		}
 	};
 
